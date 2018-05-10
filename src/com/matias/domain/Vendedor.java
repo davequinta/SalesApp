30595b9538317db3ef36bde4a020d67805f10586
David <David@DAVE-VAIO.lan>
2018-05-09 22:16:17 -0600

    Index.jsp

--- src/com/matias/domain/Vendedor.java
+++ src/com/matias/domain/Vendedor.java
@@ -6,6 +6,47 @@ public class Vendedor {
 	private String apellido;
 	private String user;
 	private String clave;
+	public Vendedor() {
+		super();
+	}
+	public Vendedor(int id, String nombre, String apellido, String user, String clave) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.user = user;
+		this.clave = clave;
+	}
+	public int getId( ) {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getClave() {
+		return clave;
+	}
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
 	
 	
 	
--- src/com/matias/domain/Venta.java
+++ src/com/matias/domain/Venta.java
@@ -1,5 +1,53 @@
 package com.matias.domain;
 
 public class Venta {
+	private int id;
+	private String date;
+	private Cliente cliente;
+	private Vendedor vendedor;
+	private double total;
+	public Venta(int id, String date, Cliente cliente, Vendedor vendedor, double total) {
+		super();
+		this.id = id;
+		this.date = date;
+		this.cliente = cliente;
+		this.vendedor = vendedor;
+		this.total = total;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	public Vendedor getVendedor() {
+		return vendedor;
+	}
+	public void setVendedor(Vendedor vendedor) {
+		this.vendedor = vendedor;
+	}
+	public double getTotal() {
+		return total;
+	}
+	public void setTotal(double total) {
+		this.total = total;
+	}
+	
+	
+	
+	
+	
 
 }
